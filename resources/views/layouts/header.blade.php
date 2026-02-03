a2d9de373a83a3c0083a6b473bac3fbd9665aade
rommel dalisay <rommelmdalisay@gmail.com>
2026-02-03 09:16:59 +0800

    reduce all

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     {{-- <a class="navbar-brand" href="{{ route('getItems') }}">larashop</a> --}}
-    <a class="navbar-brand" href="}">larashop</a>
+    <a class="navbar-brand" href="/">larashop</a>
 
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
