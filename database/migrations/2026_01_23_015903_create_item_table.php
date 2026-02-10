02978b8c0a28c418aba22b19339460b636d791dd
fritzabcdefg <zizimimocha@gmail.com>
2026-02-10 10:04:15 +0800

    new

---
+++ database/migrations/2026_01_23_015903_create_item_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->id('item_id');   // primary key named item_id
+            $table->string('name');  // example column
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::resource('items', ItemController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
